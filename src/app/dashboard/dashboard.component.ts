9b55ecb354a8b9fbac52e79b6979e60aeb71c4fb
abhishek7060 <abhishek7060@gmail.com>
2021-01-22 02:10:40 +0530

    changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  public myProfession = ["FULL STACK DEVELOPER", "UI/UX DESIGNER"];
+  loopNum: number;
+  el: any;
+  toRotate: any;
+  period: number;
+  txt: string;
+  isDeleting: boolean;
   constructor() { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    var elements = document.getElementsByClassName('anim-typewriter');
+    for (var i = 0; i < elements.length; i++) {
+      if (this.myProfession) {
+        this.TxtType(elements[i], this.myProfession, 4000);
+      }
+    }
+  }
+
+  TxtType(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 4000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  tick() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
   }
 
 }
