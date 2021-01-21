a74186ef8c2e31d9785c379de0f032cf465aa84f
abhishek7060 <abhishek7060@gmail.com>
2021-01-22 00:22:17 +0530

    responsive fix 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,19 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from './common/shared.module';
 
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
